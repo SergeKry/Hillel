7f75ea0e043eb631f9c36b275b8091c5dcde2e4a
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-01-27 14:36:17 +0200

    Repo creation and basic file push

--- LessonProject/Hillel/Lesson02.py
+++ LessonProject/Hillel/Lesson02.py
@@ -1,5 +1,5 @@
 users_text = input('Enter your text here: ')
-text_modified1 = users_text[1::2].upper()
+text_modified1 = users_text[1::2]
 text_modified2 = users_text[::-1].upper()
 print(text_modified1)
 print(text_modified2)
