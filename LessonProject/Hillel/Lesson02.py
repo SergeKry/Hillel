8f69b5dfa3575587edc046762cb6b883b9dc4a3d
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-01-27 14:55:46 +0200

    a line with original text output added

--- LessonProject/Hillel/Lesson02.py
+++ LessonProject/Hillel/Lesson02.py
@@ -1,5 +1,6 @@
 users_text = input('Enter your text here: ')
 text_modified1 = users_text[1::2]
 text_modified2 = users_text[::-1].upper()
+print(f'Original text: {users_text}')
 print(text_modified1)
 print(text_modified2)
