35807d5e12a6fecdfb4a35fa39ba9be648222bfb
SergeKry <krivosheevsrg@gmail.com>
2024-02-03 00:17:24 +0200

    Homework 4 added

---
+++ LessonProject/Hillel/Lesson03/HW4.py
@@ -0,0 +1,18 @@
+while True:
+    name = input("What's your name? ")
+    age = input("What's your age? ")
+    if not age.isdigit() or int(age) == 0:
+        print("Помилка, повторіть введення")
+    elif int(age) < 10:
+        print(f'Привіт, шкет {name.capitalize()}')
+    elif int(age) <= 18:
+        print(f'Як справи, {name.capitalize()}?')
+    elif int(age) < 100:
+        print(f'Що бажаєте, {name.capitalize()}')
+    else:
+        print(f'{name.capitalize()}, ви брешете - у наш час стільки не живуть...')
+    want_proceed = input('Бажаєте вийти? (Д/Y): ')
+    if want_proceed.upper() == 'Y' or want_proceed.upper() == 'Д':
+        break
+    else:
+        continue
