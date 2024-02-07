59b06562379aef4342efdbe96ddc29138dacf11d
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-02-07 16:01:08 +0200

    HW7 added

--- LessonProject/Hillel/Lesson05/HW7.py
+++ LessonProject/Hillel/Lesson05/HW7.py
@@ -5,11 +5,11 @@ chess_players = {
 "Caruana, Fabiano": 1792,
 "Nepomniachtchi, Ian": 2773
 }
-chess_players_reverse = {}
+new_chess_players = {}
 for k in chess_players:
     v = chess_players[k]
     if v > 2000:
         key_split = k.split(',')
         k = key_split[0]+key_split[1][:2]+'.'
-        chess_players_reverse.update({v: k})
-print(chess_players_reverse)
+        new_chess_players.update({v: k})
+print(new_chess_players)
