dace1aec3bc9871f154a548b9ebb7abe7b3d63e9
SergeKry <krivosheevsrg@gmail.com>
2024-02-10 01:25:41 +0200

    number of tries is calculated automatically based on binary search rules

--- LessonProject/Hillel/HW08.py
+++ LessonProject/Hillel/HW08.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 def get_int(text):
@@ -12,20 +13,9 @@ def get_int(text):
     return integer
 
 
-def get_tries(game_range):
-    while True:
-        tries = get_int('How many tries would you like to have?: ')
-        if tries < game_range:
-            break
-        else:
-            print('Number of tries should be less than game range. No cheating allowed')
-            continue
-    return tries
-
-
 def start_game(tries, answer, game_range):
     result = False
-    print(f'\nTHE GAME BEGINS\nMy number is somewhere between 1 and {game_range}\n')
+    print(f'\nTHE GAME BEGINS\nMy number is somewhere between 1 and {game_range}. You have {tries} tries')
     while tries > 0:
         guess = get_int('Enter your guess: ')
         tries -= 1
@@ -42,10 +32,11 @@ def start_game(tries, answer, game_range):
 
 
 users_range = get_int('What is maximum number that computer can choose?: ')
-user_tries = get_tries(users_range)
+user_tries = int(math.log2(users_range))
 computer_choice = random.randrange(1, users_range)
 game_result = start_game(user_tries, computer_choice, users_range)
 if game_result:
     print('\nYou win!!!')
 else:
     print(f'\nYou loose. Correct answer was {computer_choice}')
+
