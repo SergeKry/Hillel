04f0652c54bc54cc8bc46e4a42609b99e4ebac3a
SergeKry <krivosheevsrg@gmail.com>
2024-02-10 02:22:07 +0200

    added protection if user's guess is higher than game range

--- LessonProject/Hillel/HW08.py
+++ LessonProject/Hillel/HW08.py
@@ -2,7 +2,7 @@ import random
 import math
 
 
-def get_int(text, parameter, error):
+def get_int(text, error, parameter=0):
     while True:
         integer = input(f'{text}')
         if integer.isdigit() and int(integer) > parameter:
@@ -17,13 +17,17 @@ def start_game(tries, answer, game_range):
     result = False
     print(f'\nTHE GAME BEGINS\nMy number is somewhere between 1 and {game_range}. Number of tries: {tries}')
     while tries > 0:
-        guess = get_int('Enter your guess: ', 0, 'Wrong input. Please enter a positive integer')
+        guess = get_int('Enter your guess: ', 'Wrong input. Please enter a positive integer')
         tries -= 1
         if guess == answer:
             result = True
             break
         elif guess != answer and tries == 0:
             break
+        elif guess > game_range:
+            tries += 1
+            print('Your number is bigger than the range you chose. Be careful')
+            continue
         elif guess < answer:
             print(f'Your guess is smaller. {tries} tries left')
         else:
@@ -31,8 +35,8 @@ def start_game(tries, answer, game_range):
     return result
 
 
-range_error = 'Maximum number cannot be so small. Make your range wider'
-users_range = get_int('What is maximum number that computer can choose?: ', 1, range_error)
+range_error = 'Input error. This should be an integer bigger than 2'
+users_range = get_int('What is maximum number that computer can choose?: ', range_error, 1)
 user_tries = int(math.log2(users_range))
 computer_choice = random.randrange(1, users_range)
 game_result = start_game(user_tries, computer_choice, users_range)
