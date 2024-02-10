e2111548ecf6f792a05afe3353ee6f04846aefb5
SergeKry <krivosheevsrg@gmail.com>
2024-02-10 02:03:24 +0200

    refactoring of get_int()

--- LessonProject/Hillel/HW08.py
+++ LessonProject/Hillel/HW08.py
@@ -2,32 +2,22 @@ import random
 import math
 
 
-def get_int(text):
+def get_int(text, parameter, error):
     while True:
         integer = input(f'{text}')
-        if integer.isdigit() and int(integer) > 0:
+        if integer.isdigit() and int(integer) > parameter:
             integer = int(integer)
             break
         else:
-            print('Wrong input. Please enter a positive integer')
+            print(error)
     return integer
-
-
-def get_range(text):
-    while True:
-        desired_range = get_int(text)
-        if desired_range > 1:
-            break
-        else:
-            print('Maximum number cannot be so small. Make your range wider')
-    return desired_range
 
 
 def start_game(tries, answer, game_range):
     result = False
     print(f'\nTHE GAME BEGINS\nMy number is somewhere between 1 and {game_range}. Number of tries: {tries}')
     while tries > 0:
-        guess = get_int('Enter your guess: ')
+        guess = get_int('Enter your guess: ', 0, 'Wrong input. Please enter a positive integer')
         tries -= 1
         if guess == answer:
             result = True
@@ -41,7 +31,8 @@ def start_game(tries, answer, game_range):
     return result
 
 
-users_range = get_range('What is maximum number that computer can choose?: ')
+range_error = 'Maximum number cannot be so small. Make your range wider'
+users_range = get_int('What is maximum number that computer can choose?: ', 1, range_error)
 user_tries = int(math.log2(users_range))
 computer_choice = random.randrange(1, users_range)
 game_result = start_game(user_tries, computer_choice, users_range)
@@ -49,4 +40,3 @@ if game_result:
     print('\nYou win!!!')
 else:
     print(f'\nYou loose. Correct answer was {computer_choice}')
-
