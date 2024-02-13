1253ec30352744922574c146d781cc02095ee1f6
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-02-13 02:04:22 +0200

    Basic Phil_kotbot implementation

--- LessonProject/Kotbot/main.py
+++ LessonProject/Kotbot/main.py
@@ -4,4 +4,40 @@ from telebot import types
 api_token = '6489865740:AAFBZXIOZR7B8MqT2ilnUzgFU0rrSooTEAc'
 bot = telebot.TeleBot(api_token)
 
-status = 'XX'
+status = '\U0000274c \U0000274c'
+
+markup = types.ReplyKeyboardMarkup()
+itembtn_status = types.KeyboardButton('/status')
+itembtn_breakfast = types.KeyboardButton('/breakfast')
+itembtn_dinner = types.KeyboardButton('/dinner')
+markup.row(itembtn_status)
+markup.row(itembtn_breakfast, itembtn_dinner)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "\U0001F63A Этот бот не даст тебе быть обманутым\nФилимончик покушал?", reply_markup=markup)
+
+
+@bot.message_handler(commands=['status'])
+def get_status(message):
+	bot.reply_to(message, f'Статус Филимончика: {status}')
+
+
+@bot.message_handler(commands=['breakfast', 'dinner'])
+def get_meal(message):
+	global status
+	if message.text == '/breakfast':
+		status = '\U00002705 \U0000274c'
+		bot.send_message(message.from_user.id, f'{status}\nФилимончик позавтракал. Спасибо, что кормишь его \U0001F63B')
+	elif message.text == '/dinner':
+		status = '\U00002705 \U00002705'
+		bot.send_message(message.from_user.id, f'{status}\nФилимончик поужинал. Спасибо, что кормишь его \U0001F63B')
+
+
+@bot.message_handler(content_types=['text'])
+def get_keyboard(message):
+	bot.send_message(message.from_user.id, f'{status}\n"Филимончик покушал?', reply_markup=markup)
+
+
+bot.infinity_polling()
