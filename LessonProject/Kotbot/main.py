226a1c47d8d17f1756dbc926deeaa3e49ad87e89
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-02-13 10:48:17 +0200

    Kot is hungry every morning

--- LessonProject/Kotbot/main.py
+++ LessonProject/Kotbot/main.py
@@ -1,9 +1,11 @@
 import telebot
 from telebot import types
+from datetime import date
 
 api_token = '6489865740:AAFBZXIOZR7B8MqT2ilnUzgFU0rrSooTEAc'
 bot = telebot.TeleBot(api_token)
 
+status_updated = None
 status = '\U0000274c \U0000274c'
 
 markup = types.ReplyKeyboardMarkup()
@@ -21,17 +23,23 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['status'])
 def get_status(message):
+	if status_updated != date.today():
+		global status
+		status = '\U0000274c \U0000274c'
 	bot.reply_to(message, f'Статус Филимончика: {status}')
 
 
 @bot.message_handler(commands=['breakfast', 'dinner'])
 def get_meal(message):
 	global status
+	global status_updated
 	if message.text == '/breakfast':
 		status = '\U00002705 \U0000274c'
+		status_updated = date.today()
 		bot.send_message(message.from_user.id, f'{status}\nФилимончик позавтракал. Спасибо, что кормишь его \U0001F63B')
 	elif message.text == '/dinner':
 		status = '\U00002705 \U00002705'
+		status_updated = date.today()
 		bot.send_message(message.from_user.id, f'{status}\nФилимончик поужинал. Спасибо, что кормишь его \U0001F63B')
 
 
