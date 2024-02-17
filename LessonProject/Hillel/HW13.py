4a3b6456d268e0b6b797b3daf2fc0fd941988ca9
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-02-17 12:43:57 +0200

    Add HW12

---
+++ LessonProject/Hillel/HW13.py
@@ -0,0 +1,33 @@
+import functools
+from datetime import datetime
+
+
+def time_measure(func):
+    def wrapper():
+        now = datetime.now()
+        func()
+        print(f'measured time: {datetime.now() - now}\n')
+    return wrapper
+
+
+@time_measure
+def iteration_with_loop():
+    numbers = range(1, test_number + 1)
+    result = 0
+    for i in numbers:
+        if i % 3:
+            result += i ** 3
+    print(f'{result} loop')
+
+
+@time_measure
+def iteration_with_reduce():
+    numbers = range(1, test_number + 1)
+    result = functools.reduce(lambda a, b: a + b**3 if b % 3 else a + 0, numbers, 0)
+    print(f'{result} reduce')
+
+
+test_number = 5555555
+
+iteration_with_loop()
+iteration_with_reduce()
