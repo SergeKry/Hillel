9be0b296e735ae7ba7ed4baf23776c804ada6013
Serge_Kryvosheiev <krivosheevsrg@gmail.com>
2024-03-07 14:27:14 +0200

    HW24 added

---
+++ LessonProject/Hillel/HW24.py
@@ -0,0 +1,12 @@
+class String(str):
+
+    def __add__(self, other):
+        return String(str(self) + str(other))
+
+    def __sub__(self, other):
+        if str(other) in str(self):
+            return String(str(self).replace(str(other), '', 1))
+        else:
+            return String(self)
+
+
